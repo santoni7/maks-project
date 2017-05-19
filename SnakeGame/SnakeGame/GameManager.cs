21093c9651b77152d855e3a197f2ee43831a4726
Anton Sakhniuk <santoni7win8@gmail.com>
2017-05-19 14:39:58 +0300

    Game manager skeleton

---
+++ SnakeGame/SnakeGame/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    public class GameManager
+    {
+        private List<Cell> snake;
+        private List<FoodCell> food;
+        private HashSet<WallCell> walls;
+
+        public void Update(int dt)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+        public void Draw(int dt, Graphics canvas)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+
+        class Cell
+        {
+            Point position;
+            // ...
+        }
+        class FoodCell : Cell
+        {
+
+        }
+        class WallCell : Cell
+        {
+
+        }
+    }
+    public class InputSingletone
+    {
+        private InputSingletone _instance;
+
+        public InputSingletone Instance { get => _instance != null ? _instance : new InputSingletone(); }
+
+        private InputSingletone() { }
+
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+        public bool isKeyPressed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
